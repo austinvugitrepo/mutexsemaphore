30457d0f163c931388601e5b882f45b46920dda8
Austin <freeluck.v.i.p999@gmail.com>
2025-11-13 13:31:10 -0600

    defined my insert and remove buffer item

--- proj3.c
+++ proj3.c
@@ -7,7 +7,54 @@
 
 char **arg;
 int totalargs;
-int word_counter = 0;
+ 
+pthread_mutex_t mutex;
+sem_t empsem;          // for the mutex and semaphore
+sem_t fullsem;
+
+buffer_item bufferarr[BUFFER_SIZE]; //array declaration
+
+int in = 0;  // indexes for the empty and full semaphores
+int out = 0;
+
+void* prodthr(void* argument); //thread func prototypes
+void* consthr(void* argument); 
+
+bool buffer_insert_item( buffer_item item ) {
+
+sem_wait(&empsem);  //acquiring the mutex and semaphore
+pthread_mutex_lock(&mutex);
+
+bufferarr[in] = item; // circular
+in = (in + 1) % BUFFER_SIZE;
+
+pthread_mutex_unlock(&mutex); //releasing lock and semaphore
+sem_post(&fullsem);
+
+ return true; // return the bool
+}
+
+bool buffer_remove_item( buffer_item *item ) {
+
+sem_wait(&fullsem);  //acquiring the mutex and semaphore
+pthread_mutex_lock(&mutex);
+
+*item = bufferarr[out]; // circular but inverse to remove
+out = (out + 1) % BUFFER_SIZE;
+
+pthread_mutex_unlock(&mutex); //releasing lock and semaphore
+sem_post(&empsem);
+
+ return true; // return the bool
+}
+
+
+
+
+
+
+
+bool buffer_remove_item( buffer_item *item );
 
 int main( int argc, char *argv[] ) {  // accepting command args here
 
@@ -17,15 +64,11 @@ int main( int argc, char *argv[] ) {  // accepting command args here
  pthread_t prodcer; // for the producer and consumer threads
  pthread_t consmer;
  
- pthread_mutex_t mutex;
- sem_t empsem;          // for the mutex and semaphore
- sem_t fullsem;
- 
  pthread_mutex_init( &mutex, NULL);  //initialization
  sem_init(&empsem, 0, BUFFER_SIZE);
  sem_init(&fullsem, 0, 0);
 
- pthread_create(&prodcer, NULL, prodthr, NULL); //creating producer and consimer threads
+ pthread_create(&prodcer, NULL, prodthr, NULL); //creating producer and consumer threads
  pthread_create(&consmer, NULL, consthr, NULL);
  
  pthread_join(prodcer, NULL); //threads end here
